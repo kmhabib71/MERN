5bb7c9d7ba30bd0f386463282c79f311d49d6d0e
km Habib <km.habibs@gmail.com>
2024-04-14 22:59:37 +0600

    register complete

--- server/controller/AuthController.js
+++ server/controller/AuthController.js
@@ -1,9 +1,55 @@
+const { User } = require("../model/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const register = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  console.log("Email is: ", email);
-  console.log("Password is: ", password);
-  res.status(200).json({ message: "Registration Successfull!" });
+    const emailRegex = /^\S+@\S+\.\S+/;
+    if (!emailRegex.test(email)) {
+      return res
+        .status(400)
+        .json({ error: "INVALID_EMAIL", message: "Invalid Email" });
+    }
+
+    const passwordRegex =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!passwordRegex.test(password)) {
+      return res.status(400).json({
+        error: "INVALID_PASSWORD",
+        message:
+          "Password must contain at least 8 characters, one uppercase, one lowercase, one number and one special character",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingEmail = await User.findOne({ email });
+
+    if (existingEmail) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+    });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, "your_secret_key", {
+      expiresIn: "7d",
+    });
+
+    res.cookie("mern-engine", token, {
+      httpOnly: true,
+      maxAge: 7 * 24 * 60 * 60 * 1000,
+    });
+    console.log("token :", token);
+    res.status(200).json({ message: "Registration Successful", token: token });
+  } catch (error) {
+    return res.status(500).json({ error: "Failed to register user" });
+  }
 };
 
 module.exports = {
